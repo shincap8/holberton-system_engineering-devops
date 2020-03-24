006453cb0ca3b6da73f28cdd08cc994e0bf9d6af
shincap8 <shincap8@gmail.com>
2020-03-24 13:58:10 +0000

    Add flag to what first had

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,4 @@
 #manifest that kills a process named killmenow.
 exec { 'killmenow':
-  path    => ['/usr/bin', '/bin', '/usr/local/bin'],
-  command => 'pkill -f killmenow'
+  command => '/usr/bin/pkill -f killmenow'
 }
