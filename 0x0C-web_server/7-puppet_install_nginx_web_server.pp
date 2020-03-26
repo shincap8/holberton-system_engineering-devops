c78b8c5a633e62660acdc20dc43625d17fc0a823
shincap8 <shincap8@gmail.com>
2020-03-26 00:26:37 +0000

    Make the name shorter

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 #install and configure an Nginx server using Puppet instead of Bash
-exec{ 'install and configure the Nginx server':
+exec{ 'install':
   command  => "apt-get update && apt-get -y install nginx && echo 'Holberton School' > /var/www/html/index.html && sed -i '43i location /redirect_me{\nrewrite ^/(.*)$ https://www.youtube.com/watch?v=EgBJmlPo8Xw permanent;\n}\n' /etc/nginx/sites-available/default && service nginx start",
   provider => 'shell'
 }
