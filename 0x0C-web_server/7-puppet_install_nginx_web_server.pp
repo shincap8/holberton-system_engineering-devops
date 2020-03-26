a16165051a099f9e6637579992415d56ae5f5720
shincap8 <shincap8@gmail.com>
2020-03-26 00:23:47 +0000

    Once more to the command and provider changing the order

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,5 @@
 #install and configure an Nginx server using Puppet instead of Bash
-#install and configure an Nginx server using Puppet instead of Bash
-exec{ 'update':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => 'sudo apt-get update',
-}
-exec{ 'install':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => 'sudo apt-get -y install nginx',
-}
-exec{ 'Holberton':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => "echo -e 'Holberton School' > /var/www/html/index.html",
-}
-exec{ 'Redirect':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => "sed -i '43i location /redirect_me{\nrewrite ^/(.*)$ https://www.youtube.com/watch?v=EgBJmlPo8Xw permanent;\n}\n' /etc/nginx/sites-available/default",
-}
-exec{ 'Start':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => 'service nginx start',
+exec{ 'install and configure the Nginx server':
+  command  => "apt-get update && apt-get -y install nginx && echo 'Holberton School' > /var/www/html/index.html && sed -i '43i location /redirect_me{\nrewrite ^/(.*)$ https://www.youtube.com/watch?v=EgBJmlPo8Xw permanent;\n}\n' /etc/nginx/sites-available/default && service nginx start",
+  provider => 'shell'
 }
