6317f3e283212e720ed756a1995c2a539764c5fc
shincap8 <shincap8@gmail.com>
2020-03-26 00:36:33 +0000

    Remove some \n I hope this works!

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 #install and configure an Nginx server using Puppet instead of Bash
-exec{ 'install':
-  command  => "apt-get update && apt-get -y install nginx && echo 'Holberton School' > /var/www/html/index.html && sed -i '43i location /redirect_me{\nrewrite ^/(.*)$ https://www.youtube.com/watch?v=EgBJmlPo8Xw permanent;\n}\n' /etc/nginx/sites-available/default && service nginx start",
-  provider => 'shell'
+exec { 'install and configure':
+  command  => "apt-get update && apt-get -y install nginx && echo -e 'Holberton School' > /var/www/html/index.html && sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=EgBJmlPo8Xw permanent;' /etc/nginx/sites-available/default && service nginx start",
+  provider => 'shell',
 }
