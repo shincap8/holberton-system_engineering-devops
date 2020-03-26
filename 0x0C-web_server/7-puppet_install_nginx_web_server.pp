42d89fbce020d63fa03af79370adf596d3e9c657
shincap8 <shincap8@gmail.com>
2020-03-26 00:16:48 +0000

    remove path

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,5 @@
 #install and configure an Nginx server using Puppet instead of Bash
 exec{ 'install and configure the Nginx server':
-  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   provider => 'shell',
   command  => "sudo apt-get update && sudo apt-get -y install nginx && echo -e 'Holberton School' > /var/www/html/index.html && sed -i '43i location /redirect_me{\nrewrite ^/(.*)$ https://www.youtube.com/watch?v=EgBJmlPo8Xw permanent;\n}\n' /etc/nginx/sites-available/default && service nginx start"
 }
