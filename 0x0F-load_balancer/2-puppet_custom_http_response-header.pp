58d9905c60f1e2c50337e36cefe8299fea8acb12
shincap8 <shincap8@gmail.com>
2020-04-03 19:29:24 +0000

    Change variable adbance task

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,5 +1,5 @@
 #automate the task of creating a custom HTTP header response
 exec { 'configure HTTP':
-  command  => "apt-get update && apt-get -y install nginx && echo -e 'Holberton School' > /var/www/html/index.html && sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=EgBJmlPo8Xw permanent;' /etc/nginx/sites-available/default &&sed -i '22i add_header X-Served-By $HOSTNAME;\n' /etc/nginx/nginx.conf && service nginx start",
+  command  => "apt-get update && apt-get -y install nginx && echo -e 'Holberton School' > /var/www/html/index.html && sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=EgBJmlPo8Xw permanent;' /etc/nginx/sites-available/default && sed -i '22i add_header X-Served-By $hostname;' /etc/nginx/nginx.conf && service nginx start",
   provider => 'shell',
 }
