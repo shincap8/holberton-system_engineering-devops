30f9e694c5f940dac815490ae7c9d46da43f24c5
shincap8 <shincap8@gmail.com>
2020-05-07 17:19:52 +0000

    Task 0 and 1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and returns the
+number of subscribers for a given subreddit"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """method to return the number of suscribers"""
+    url = "https://reddit.com/r/" + subreddit + ".json"
+    headers = {
+        'user-agent': 'Chrome/81.0.4044.129'
+    }
+    reddit = requests.get(url, headers=headers).json()
+    if 'error' in reddit:
+        return 0
+    if type(reddit['data']['children'][0]['data']['subreddit_subscribers']) == int:
+        return reddit['data']['children'][0]['data']['subreddit_subscribers']
+    return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and prints the titles of
+the first 10 hot posts listed for a given subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """method to return the number of suscribers"""
+    url = "https://reddit.com/r/" + subreddit + "/hot.json"
+    headers = {
+        'user-agent': 'Chrome/81.0.4044.129'
+    }
+    reddit = requests.get(url, headers=headers).json()
+    if 'error' in reddit:
+        print (None)
+        return
+    children = reddit['data']['children']
+    count = 0
+    for child in children:
+        if count == 10:
+            return
+        print(child['data']['title'])
+        count += 1
