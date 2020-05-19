f388c11fe29fa0931c2a7413e85a5fb4d2810c76
shincap8 <shincap8@gmail.com>
2020-05-19 17:51:22 +0000

    Debugging #3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+#Fix name file in wp-settings.php
+exec {'name_file_fixed':
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command => "sed -i 's/phpp/php/g' /var/www/hemlt/wp-settings.php"
+}
